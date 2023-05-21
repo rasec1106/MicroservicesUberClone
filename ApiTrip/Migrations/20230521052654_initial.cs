36733d4bae4ae2e68dda65b4570acb667f5d4c9a
rasec1106 <elch.herrera@gmail.com>
2023-05-21 00:34:51 -0500

    Add migrations

---
+++ ApiTrip/Migrations/20230521052654_initial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTrip.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "drivers",
+                columns: table => new
+                {
+                    driverId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    licenseNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_drivers", x => x.driverId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    userId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.userId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "trips",
+                columns: table => new
+                {
+                    tripId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    userId = table.Column<int>(type: "int", nullable: false),
+                    driverId = table.Column<int>(type: "int", nullable: false),
+                    pickupLocation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    destinationLocation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    pickupTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_trips", x => x.tripId);
+                    table.ForeignKey(
+                        name: "FK_trips_drivers_driverId",
+                        column: x => x.driverId,
+                        principalTable: "drivers",
+                        principalColumn: "driverId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_trips_users_userId",
+                        column: x => x.userId,
+                        principalTable: "users",
+                        principalColumn: "userId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_trips_driverId",
+                table: "trips",
+                column: "driverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_trips_userId",
+                table: "trips",
+                column: "userId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "trips");
+
+            migrationBuilder.DropTable(
+                name: "drivers");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
