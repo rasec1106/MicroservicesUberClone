73b24df9fc19a6c0e763e78322f5cbd4ae3ba0ae
rasec1106 <elch.herrera@gmail.com>
2023-05-20 23:44:26 -0500

    Add models

--- ApiTrip/Models/Trip.cs
+++ ApiTrip/Models/Trip.cs
@@ -1,6 +1,25 @@
-﻿namespace ApiTrip.Models
+﻿using System.Net.NetworkInformation;
+
+namespace ApiTrip.Models
 {
     public class Trip
     {
+        public int tripId { get; set; }
+        public int userId { get; set; }
+        public int driverId { get; set; }
+        public string? pickupLocation { get; set; }
+        public string? destinationLocation { get; set; }
+        public DateTime pickupTime { get; set; }
+        public TripStatus status { get; set; }
+        public Driver? driver { get; set; }
+        public User? user { get; set; }
+    }
+
+    public enum TripStatus
+    {
+        Pending,
+        InProgress,
+        Completed,
+        Cancelled
     }
 }
