e94f2ede4a77725ffa0763d0f7707ac10dd926e7
rasec1106 <elch.herrera@gmail.com>
2023-05-20 23:54:47 -0500

    Add Db context

--- ApiTrip/DbContexts/AppDbContext.cs
+++ ApiTrip/DbContexts/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace ApiTrip.DbContexts
+﻿using ApiTrip.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTrip.DbContexts
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Trip> trips { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<Driver> drivers { get; set; }
     }
 }
