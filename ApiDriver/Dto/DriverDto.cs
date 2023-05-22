b6ae0d24b733ddff2beff06570831ef87a7e89ec
fernandobarahonahuayta <fernandobarahonahuayta@gmail.com>
2023-05-21 21:47:03 -0500

    primer avance

---
+++ ApiDriver/Dto/DriverDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiDriver.Dto
+{
+    public class DriverDto
+    {
+        public int condId { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? nroTele { get; set; }
+        public string? correo { get; set; }
+
+    }
+}
---
+++ ApiDriver/Repository/IRepositoryDriver.cs
@@ -0,0 +1,15 @@
+﻿using ApiDriver.Dto;
+
+namespace ApiDriver.Repository
+{
+    public interface IRepositoryDriver
+    {
+
+        Task<IEnumerable<DriverDto>> GetDrivers();
+        Task<DriverDto> GetDriverById(int condId);
+        Task<DriverDto> CreateDriver(DriverDto driverDto);
+        Task<DriverDto> UpdateDriver(DriverDto driverDto);
+        Task<bool> DeleteDriver(int condId);
+
+    }
+}
